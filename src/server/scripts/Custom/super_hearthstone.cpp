eb697a09773ed6da99415ffe8763e7370d6dbbb4
Matheus Martins <matheusmcrds@gmail.com>
2017-03-08 16:39:35 -0300

    super_hearthstone teleporting
    gossip now teleports to location in selected item
    
    todo:
    
    - add new waypoint in DB when speaking to InnKeeper

--- src/server/scripts/Custom/super_hearthstone.cpp
+++ src/server/scripts/Custom/super_hearthstone.cpp
@@ -6,15 +6,53 @@ class super_hearthstone : ItemScript
 public:
 	super_hearthstone() : ItemScript("super_hearthstone") {}
 
-	// more code .. 
+	// may return null_ptr
+	const MapEntry* GetMapEntryFromMapId(uint32 mapId) {
+		return sMapStore.LookupEntry(mapId);
+	}
+
+	const std::string GetMapNameFromMapId(uint32 mapId, LocaleConstant locale = LOCALE_enUS) {
+		auto entry = GetMapEntryFromMapId(mapId);
+		if (!entry)
+			return "UNKNOWN_MAPID_" + std::to_string(mapId);
+		return entry->name[locale];
+	}
+
+	// may return null_ptr
+	const AreaTableEntry* GetAreaTableEntryFromAreaId(uint32 areaId) {
+		return sAreaTableStore.LookupEntry(areaId);
+	}
+
+	// may return null_ptr
+	const uint32 GetAreaIdFromWorldLocation(WorldLocation w) {
+		return MapManager::instance()->GetAreaId(w.m_mapId, w.m_positionX, w.m_positionY, w.m_positionZ);
+	}
+
+	// may return null_ptr
+	const AreaTableEntry* GetAreaTableEntryFromWorldLocation(WorldLocation w) {
+		auto areaId = GetAreaIdFromWorldLocation(w);
+		return GetAreaTableEntryFromAreaId(areaId);
+	}
+
+	const std::string GetAreaNameFromAreaId(uint32 areaId, LocaleConstant locale = LOCALE_enUS) {
+		auto entry = GetAreaTableEntryFromAreaId(areaId);
+		if (!entry)
+			return "UNKNOWN_AREAID_" + std::to_string(areaId);
+		return std::string(entry->area_name[locale]);
+	}
+
+	const std::string GetAreaNameFromWorldLocation(WorldLocation w, LocaleConstant locale = LOCALE_enUS) {
+		auto areaId = GetAreaIdFromWorldLocation(w);
+		return GetAreaNameFromAreaId(areaId, locale);
+	}
 
 	std::vector<WorldLocation> GetPlayerWaypoints(Player* player) {
 
 		auto waypoints = std::vector<WorldLocation>();
 
-		std::string query = "SELECT map, x, y, z, o FROM super_hearthstone_waypoints WHERE character_guid = ";
+		std::string query = "SELECT mapId, x, y, z, o FROM super_hearthstone_waypoints WHERE character_guid = ";
 		query += std::to_string(player->GetSession()->GetGUIDLow());
-		query += ";";
+		query += " ORDER BY mapId;";
 
 		auto res = CharacterDatabase.Query(query.c_str());
 
@@ -23,13 +61,20 @@ public:
 			{
 				Field* field = res->Fetch();
 
-				uint16 id = field[0].GetUInt16();
+				uint16 mapId = field[0].GetUInt16();
+
+				if (!GetMapEntryFromMapId(mapId))
+					continue;
+
 				float x = field[1].GetFloat();
 				float y = field[2].GetFloat();
 				float z = field[3].GetFloat();
 				float o = field[4].GetFloat();
 
-				auto location = WorldLocation(id, x, y, z, o);
+				auto location = WorldLocation(mapId, x, y, z, o);
+
+				if (!GetAreaTableEntryFromWorldLocation(location))
+					continue;
 
 				waypoints.push_back(location);
 
@@ -42,28 +87,38 @@ public:
 	bool OnUse(Player* player, Item* item, SpellCastTargets const& /*targets*/) {
 
 		auto locale = player->GetSession()->GetSessionDbLocaleIndex();
+		auto waypoints = GetPlayerWaypoints(player);
 
 		ClearGossipMenuFor(player);
 
-		auto waypoints = GetPlayerWaypoints(player);
+		auto i = 1;
+		for (auto w : waypoints) {
 
-		for (auto w = waypoints.begin(); w < waypoints.end(); w++) {
-			auto mapEntry = sMapStore.LookupEntry(w->m_mapId);
-			if (!mapEntry) continue;
-			auto mapName = std::string(mapEntry->name[locale]);
-
-			auto areaId = MapManager::instance()->GetAreaId(w->m_mapId, w->m_positionX, w->m_positionY, w->m_positionZ);
-			auto areaEntry = sAreaTableStore.LookupEntry(areaId);
-			if (!areaEntry) continue;
-			auto areaName = std::string(areaEntry->area_name[locale]);
+			auto mapName = GetMapNameFromMapId(w.m_mapId, locale);
+			auto areaName = GetAreaNameFromWorldLocation(w, locale);
 
 			AddGossipItemFor(player, 0, areaName + " (" + mapName + ")",
-							 GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+				GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + i);
+
+			i++;
 		}
 
 		SendGossipMenuFor(player, 135555, item->GetGUID());
 
 		return true;
+	}
+
+	void OnGossipSelect(Player* player, Item* /*item*/, uint32 /*sender*/, uint32 action) {
+
+		ClearGossipMenuFor(player);
+
+		auto waypoints = GetPlayerWaypoints(player);
+		auto choice = action - GOSSIP_ACTION_INFO_DEF - 1;
+		auto waypoint = waypoints[choice];
+
+		player->TeleportTo(waypoint);
+
+		CloseGossipMenuFor(player);
 	}
 
 	bool OnRemove(Player* /*player*/, Item* /*item*/) {
